547aafb3493ee4c1966c8d2b354ce03989f945c8
I550118 <julian.schambeck@sap.com>
2023-08-07 14:45:00 +0200

    configure router to forward to vite dev server locall and use local build for prod

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    hmr: {
+      port: 5174,
+    },
+  },
+  build: {
+    outDir: "../router/dist",
+    emptyOutDir: true,
+  },
 });
